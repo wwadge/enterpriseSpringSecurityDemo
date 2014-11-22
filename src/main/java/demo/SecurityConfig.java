425cfb6d6c9e6e33f8e2a905c582ca485d12fd38
Wallace Wadge <wwadge@gmail.com>
2014-11-22 10:56:04 +0100

    Completely overriding spring boot defaults to have our config

---
+++ src/main/java/demo/SecurityConfig.java
@@ -0,0 +1,22 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by wwadge on 22/11/14.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("password").roles("USER");
+    }
+}
